cf45a83c2dc1e67d0e0e1d29688109b172cd7f2c
timlv <kidyfirst@126.com>
2015-10-08 15:50:47 +0800

    update the bower.json

--- src/comro.js
+++ src/comro.js
@@ -2,6 +2,6 @@ if(typeof define!="undefined") {
     define(function (require, exports, module) {
         module.exports = exports = window["@Alias"];
     });
-}if (typeof module != "undefined" && module.exports) {
+}else if (typeof module != "undefined" && module.exports) {
     module.exports = window["@Alias"];
 }
