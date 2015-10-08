bca29b00ae3b6e21a9283d97782025117d58d2c1
timlv <kidyfirst@126.com>
2015-10-08 20:28:45 +0800

    remove delayRun

--- src/fn.js
+++ src/fn.js
@@ -1,12 +1,28 @@
+var nextFrame = (function() {
+    return window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.oRequestAnimationFrame ||
+        window.msRequestAnimationFrame ||
+        function(callback) { return setTimeout(callback, 1); };
+})();
 var fn = madutil.fn = {
-    numberInputFix: function(ele, isReserve) {
-        var _src = ele.value,
-            _replace = _src.replace(/[^\d]/g, '');
-
-        if (_src !== _replace && !isReserve) {
-            ele.value = _replace;
+    delayRun: function (timerKey, fn, delay, object) {
+        object = object || window;
+        if (object._delayRunList) {
+            if (object._delayRunList[timerKey]) {
+                clearTimeout(object._delayRunList[timerKey]);
+                object._delayRunList[timerKey] = -1;
+            }
+        } else {
+            object._delayRunList = {};
         }
-        return _replace;
+        object._delayRunList[timerKey] = setTimeout(function () {
+            fn.call(object);
+        }, ~~delay);
+    },
+    nextFrame: function () {
+        nextFrame.apply(window, arguments);
     },
     uuid : function(len, radix){
         var CHARS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".split('');
--- util.js
+++ util.js
@@ -294,9 +294,6 @@ var jsonParse = madutil.json = {
         return ret;
     }
 };
-/**
- * Created by timlv on 15.9.15.
- */
 var nextFrame = (function() {
     return window.requestAnimationFrame ||
         window.webkitRequestAnimationFrame ||
@@ -305,32 +302,23 @@ var nextFrame = (function() {
         window.msRequestAnimationFrame ||
         function(callback) { return setTimeout(callback, 1); };
 })();
-madutil.fn.delayRun  = function (timerKey, fn, delay, object) {
-    object = object || window;
-    if (object._delayRunList) {
-        if (object._delayRunList[timerKey]) {
-            clearTimeout(object._delayRunList[timerKey]);
-            object._delayRunList[timerKey] = -1;
-        }
-    } else {
-        object._delayRunList = {};
-    }
-    object._delayRunList[timerKey] = setTimeout(function () {
-        fn.call(object);
-    }, ~~delay);
-};
-madutil.nextFrame = function(){
-    nextFrame.apply(window, arguments);
-};
 var fn = madutil.fn = {
-    numberInputFix: function(ele, isReserve) {
-        var _src = ele.value,
-            _replace = _src.replace(/[^\d]/g, '');
-
-        if (_src !== _replace && !isReserve) {
-            ele.value = _replace;
+    delayRun: function (timerKey, fn, delay, object) {
+        object = object || window;
+        if (object._delayRunList) {
+            if (object._delayRunList[timerKey]) {
+                clearTimeout(object._delayRunList[timerKey]);
+                object._delayRunList[timerKey] = -1;
+            }
+        } else {
+            object._delayRunList = {};
         }
-        return _replace;
+        object._delayRunList[timerKey] = setTimeout(function () {
+            fn.call(object);
+        }, ~~delay);
+    },
+    nextFrame: function () {
+        nextFrame.apply(window, arguments);
     },
     uuid : function(len, radix){
         var CHARS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".split('');
